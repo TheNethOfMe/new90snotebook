e5f0f2362e813725d798faa984b46fd726058c90
steampunklink <nethhorn@gmail.com>
2019-01-14 12:30:28 -0800

    added user model and register route

--- data/api/users.js
+++ data/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 // ROUTE  GET api/users/post
 // DESC   Gets Test User Route
@@ -7,6 +9,35 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({
     msg: "USERS Works"
+  });
+});
+
+// ROUTE  POST api/users/register
+// DESC   Creates a new user
+// ACCESS Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already registered." });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        console.log("USER", newUser);
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
   });
 });
 
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+// Import npm dependencies
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
+// Inport api routes
 const users = require("./data/api/users");
 const profile = require("./data/api/profile");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
 const db = require("./config/keys_dev").mongoURI;
 
+// Connect to MongoDB
 mongoose
   .connect(
     db,
@@ -15,15 +24,17 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
+// Map Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 
+// Create Port
 const PORT = process.env.PORT || 5000;
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
