643774ac9794b11ccc9bd0487723f5b4f669f0da
steampunklink <nethhorn@gmail.com>
2019-03-15 13:10:09 -0700

    added new buttons and styles to menus

--- client/src/components/common/Menu.js
+++ client/src/components/common/Menu.js
@@ -2,7 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div id="menu-display">
-    <Link to="/friendDash">Friends</Link>
+  <div id="menu__display">
+    <Link to="/dashboard">
+      <img
+        className="menu__button"
+        src="/images/DashboardButton.png"
+        alt="Dashboard"
+      />
+    </Link>
+    <Link to="/friendDash">
+      <img
+        className="menu__button"
+        src="/images/MyFriendsButton.png"
+        alt="My Friends"
+      />
+    </Link>
   </div>
 );
