95713f13eb0f3045384d890665745b2d7937598c
steampunklink <nethhorn@gmail.com>
2019-04-08 11:53:47 -0700

    prepared for deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 // Inport api routes
 const users = require("./data/api/users");
@@ -41,6 +42,15 @@ app.use("/api/profile", profile);
 app.use("/api/notification", notifications);
 app.use("/api/friends", friends);
 app.use("/api/posts", posts);
+
+// Serve Static Routes if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Create Port
 const PORT = process.env.PORT || 5000;
