392ea5e34d4cd06864f0ebb719762a389ccb088a
steampunklink <nethhorn@gmail.com>
2019-04-08 11:39:01 -0700

    updates to db for heroku deploy

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys_dev").mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
